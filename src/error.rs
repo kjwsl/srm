bd2155b9e9d15b52d014aca764393cb0e9bc525c
Ray <kjwsl@fatherslab.com>
2025-01-17 12:27:49 +0900

    update: have Error derive Debug

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,14 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
 pub struct Error {
     pub kind: ErrorKind,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.kind)
+    }
 }
 
 impl Error {
